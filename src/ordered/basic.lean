90b5bdd7fb62654d7d5d550ede17b7649472501a
ocornoc <ocornoc@protonmail.com>
2020-10-12 14:49:45 -0700

    Dimensionality renamed, now uses list

--- src/ordered/basic.lean
+++ src/ordered/basic.lean
@@ -15,12 +15,12 @@ parameters {α : Type u} [has_betweenness α]
     This is a generalization of "there are two distinct points", "for any given
     line there's a point that's not collinear to it", "for any given plane
     there's a point that's not coplanar to it", etc. -/
-def dimensionality {d : ℕ} (vs : vector α d.succ) : Prop :=
-∃ p, lin_indep p $ convex_hull {z | z ∈ vs.val}
+def lindep_to_simplex (vs : list α) : Prop :=
+∃ p, lin_indep p $ convex_hull {z | z ∈ vs}
 
 @[simp]
-theorem dimensionality.dim_def {d : ℕ} (vs : vector α d.succ) :
-  dimensionality vs = ∃ p, lin_indep p $ convex_hull {z | z ∈ vs.val} :=
+theorem lindep_to_simplex.dim_def (vs : list α) :
+  lindep_to_simplex vs = ∃ p, lin_indep p $ convex_hull {z | z ∈ vs} :=
 rfl
 
 /-- A simplex is nondegenerate iff, for every two endpoints and point `p`, if
@@ -39,7 +39,7 @@ s₁ ∪ s₂ = s ∧ (∀ p ∈ s₁, ¬ ∃ q r ∈ s₂, between q p r) ∧
 end
 
 /-- Ordered geometry, without an axiom of dimension. -/
-class {u} ordered_geo_nodim (α : Type u) extends has_betweenness α, inhabited α :=
+class {u} ordered_geo_nodim (α : Type u) extends has_betweenness α :=
 (pasch₁ {v₁ v₂ : α} (v₃ v₄ ∈ line v₁ v₂) : v₃ ≠ v₄ → v₁ ∈ line v₃ v₄)
 (pasch₂ {v₁ v₂ v₃ v₄ v₅ : α} : nondegen_simplex [v₁, v₂, v₃] → between v₂ v₃ v₄ →
   between v₃ v₅ v₂ → ∃ z ∈ line v₄ v₅, between v₁ z v₂)
@@ -49,10 +49,9 @@ class {u} ordered_geo_nodim (α : Type u) extends has_betweenness α, inhabited 
     ∨ (∃ v₁ ∈ s₂, ∀ (v₂ ∈ s₂ \ {v₁}) (v₃ ∈ s₁), between v₂ v₁ v₃))
 
 /-- Ordered geometry, indexed by a dimension `d` for which the appropriate
-    axioms of dimensionality apply for any nonempty list of points at most
-    `d+1` long. -/
+    axioms of dimensionality apply for any list of points at most `d` long. -/
 class {u} ordered_geo (α : Type u) (d : ℕ) extends ordered_geo_nodim α :=
-(dimality {d' : ℕ} (h : d' < d) (vs : vector α d'.succ) : dimensionality vs)
+(lin_simp (vs : list α) : vs.length ≤ d → lindep_to_simplex vs)
 
 /-- Ordered geometry with a finite dimension `d`. This finiteness is shown by
     the property that every non-degenerate convex hull of `d` vertices has two
@@ -64,7 +63,7 @@ class {u} ordered_geo_fin (α : Type u) (d : ℕ) extends ordered_geo α d :=
 /-- An infinite ordered geometry, with the property that the axiom of
     dimensionality holds for any dimension. -/
 class {u} ordered_geo_inf (α : Type u) extends ordered_geo_nodim α :=
-(inf_dimality {d : ℕ} (vs : vector α d.succ) : dimensionality vs)
+(lin_any_simp (vs : list α) : lindep_to_simplex vs)
 
 section
 universe u
@@ -99,10 +98,20 @@ end nondegen_simplex
 
 namespace ordered_geo
 
+instance (d : ℕ) [ordered_geo α d] : nonempty α :=
+(lin_simp [] (dec_trivial : 0 ≤ d)).rec_on $ λ w _, nonempty.intro w
+
+theorem ordered_geo_subspace {d : ℕ} [ordered_geo α d] (d' : ℕ) (h : d' ≤ d) :
+  ordered_geo α d' :=
+ordered_geo.mk $ λ _ hl, lin_simp _ $ le_trans hl h
+
+lemma ordered_geo_1d (d : ℕ) [ordered_geo α d] : ordered_geo α 0 :=
+ordered_geo_subspace _ (dec_trivial : 0 ≤ d)
+
 /-- For any point, there exists another point that's not equal to it. -/
 theorem ex_not_eq {d : ℕ} [ordered_geo α $ d + 1] (p : α) : ∃ q, p ≠ q :=
 begin
-  cases dimality (dec_trivial : 0 < d + 1) ⟨[p], dec_trivial⟩ with w h,
+  cases lin_simp [p] (dec_trivial : 1 ≤ d + 1) with w h,
   use w,
   simp only [list.mem_singleton] at h,
   specialize h p p (convex_hull.of_set rfl) (convex_hull.of_set rfl),
@@ -110,13 +119,14 @@ begin
 end
 
 instance {d : ℕ} [ordered_geo α $ d + 1] : nontrivial α :=
-⟨⟨arbitrary α, @ex_not_eq d _ _⟩⟩
+(@nonempty _ $ ordered_geo_1d d.succ).rec_on
+  $ λ p, nontrivial.mk ⟨p, @ex_not_eq d _ _⟩
 
 /-- For any line, there exists a point that's not in it. -/
 theorem ex_not_on_line {d : ℕ} [ordered_geo α $ d + 2] (p q : α) :
   ∃ r, r ∉ line p q :=
 begin
-  cases dimality (dec_trivial : 1 < d + 2) ⟨[p, q], dec_trivial⟩ with w h,
+  cases lin_simp [p, q] (dec_trivial : 2 ≤ d + 2) with w h,
   refine ⟨w, λ hw, _⟩,
   specialize h p q (convex_hull.of_set $ or.inl rfl),
   replace hw : collinear p q w := ⟨p, q, by simp, by simp, hw⟩,
@@ -126,11 +136,8 @@ end
 /-- For any plane, there exists a point that's not in it. -/
 theorem ex_not_on_plane {d : ℕ} [ordered_geo α $ d + 3] (p q r : α) :
   ∃ x, x ∉ convex_hull {y | y ∈ [p, q, r]} :=
-begin
-  cases dimality (dec_trivial : 2 < d + 3) ⟨[p, q, r], dec_trivial⟩ with w h,
-  use w,
-  exact h.not_mem_of_indep
-end
+(lin_simp [p, q, r] (dec_trivial : 3 ≤ d + 3)).rec_on
+  $ λ w h, Exists.intro w h.not_mem_of_indep
 
 end ordered_geo
 end
@@ -140,18 +147,14 @@ universe u
 parameters {α : Type u} [ordered_geo_inf α]
 namespace ordered_geo_inf
 
-def to_ordered_geo (d : ℕ) : ordered_geo α d := {
-  dimality := λ d' _, inf_dimality
-}
-
-instance ordered_geo (d : ℕ) : ordered_geo α d := to_ordered_geo d
+instance (d : ℕ) : ordered_geo α d := {lin_simp := λ l _, lin_any_simp l}
 
 /-- For any point, there exists another point that's not equal to it. -/
 theorem ex_not_eq (p : α) : ∃ q, p ≠ q :=
 @ordered_geo.ex_not_eq _ 0 (ordered_geo _) p
 
-instance : nontrivial α :=
-⟨⟨arbitrary α, ex_not_eq _⟩⟩
+instance : nonempty α := @ordered_geo.nonempty _ 0 _
+instance : nontrivial α := @ordered_geo.nontrivial _ 0 _
 
 /-- For any line, there exists a point that's not in it. -/
 theorem ex_not_on_line (p q : α) : ∃ r, r ∉ line p q :=
@@ -167,12 +170,10 @@ theorem ex_not_on_plane (p q r : α) :
 lemma ex_point_not_mem_simplex_hull (l : list α) :
   ∃ p, p ∉ convex_hull {q | q ∈ l} :=
 begin
-  cases l with hd tl, { simp },
   classical,
   by_contra h,
   push_neg at h,
-  have := @ordered_geo_inf.inf_dimality _ _ tl.length ⟨hd :: tl, by trivial⟩,
-  cases this with w hw,
+  cases lin_any_simp l with w hw,
   rcases collinear.ex_collinear_of_mem (h w) with ⟨_, _, hp, hq, hpq⟩,
   exact hw _ _ hp hq hpq.rotate
 end
@@ -183,15 +184,16 @@ theorem ex_nondegen_simplex (d : ℕ) :
 begin
   induction d with d hd, { exact ⟨vector.nil, nondegen_simplex.of_nil⟩ },
   induction d with d hd,
-    { exact ⟨⟨[arbitrary α], rfl⟩, by simp, by finish⟩ },
+    { cases ordered_geo_inf.nonempty with w,
+      exact ⟨⟨[w], rfl⟩, by simp, by finish⟩ },
   rcases hd with ⟨⟨l, h⟩, hvs⟩,
-  rcases inf_dimality ⟨l, h⟩ with ⟨p, hp⟩,
+  rcases lin_any_simp l with ⟨p, hp⟩,
   suffices : (p :: ⟨l, h⟩ : vector α _).val.nodup,
   refine ⟨vector.cons p ⟨l, h⟩, this, λ v₁ v₂ v₃ hv₁ hv₂ hv₃, _⟩,
   rw lin_indep at hp,
   have : subtype.val (p :: (⟨l, h⟩ : vector _ _)) = p :: l := rfl,
   rw [this, list.mem_cons_iff] at hv₁ hv₂ hv₃,
-  rw (rfl : subtype.val (subtype.mk l h) = l) at hp hvs,
+  rw (rfl : subtype.val (subtype.mk l h) = l) at hvs,
   rw collinear.none_collinear_iff_empty_fixed_1'' at hp,
   rw [convex_hull.is_empty_iff, set.eq_empty_iff_forall_not_mem] at hp,
   have : 0 < l.length := by rw h; exact nat.succ_pos',
@@ -210,7 +212,7 @@ theorem not_all_in_space (d : ℕ) : ¬ ∀ {vs : vector α d.succ},
 begin
   intro hvs,
   rcases ex_nondegen_simplex (d + 1) with ⟨vs, hl⟩,
-  cases ordered_geo_inf.inf_dimality vs with w hw,
+  cases ordered_geo_inf.lin_any_simp vs.val with w hw,
   rcases hvs hl w with ⟨_, _, hp, hq, hpq⟩,
   apply hw _ _ hp hq,
   rwa ←collinear.rotate_iff
